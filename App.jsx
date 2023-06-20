ff770a2defdd199eed74b5abe315e864f985c30b
Colson <colsoncalifornication@gmail.com>
2023-06-20 09:26:09 +0545

    add login form

---
+++ App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+function Contact() {
+  const password = 'swordfish';
+  const [authorized, setAuthorized] = useState(false);
+
+  function handleSubmit(e) {
+    const enteredPassword = e.target.querySelector(
+      'input[type="password"]'
+    ).value;
+    const auth = enteredPassword == password;
+    setAuthorized(auth);
+  }
+
+  const login = (
+    <form action="#">
+      <input type="password" placeholder="Password" />
+      <input type="submit" />
+    </form>
+  );
+
+  return (
+    <div id="authorization">
+      <h1>{authorized ? 'Contact' : 'Enter the Password'}</h1>
+      <ul>
+        <li>client@example.com</li>
+        <li>555.555.5555</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Contact;
